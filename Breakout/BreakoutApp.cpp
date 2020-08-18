a4f0d0411ef10cd5c583aafe7e4c95b498dcdaac
Hunter <trahan.hunter@yahoo.com>
2020-08-18 11:00:14 -0500

    New Code
    added new code to BreakoutApp.cpp

--- Breakout/BreakoutApp.cpp
+++ Breakout/BreakoutApp.cpp
@@ -2,55 +2,68 @@
 #include "Texture.h"
 #include "Font.h"
 #include "Input.h"
+#include <glm\ext.hpp>
+#include <Gizmos.h>
 
-BreakoutApp::BreakoutApp() {
+BreakoutApp::BreakoutApp() 
+{
 
 }
 
-BreakoutApp::~BreakoutApp() {
+BreakoutApp::~BreakoutApp() 
+{
 
 }
 
-bool BreakoutApp::startup() {
-	
+bool BreakoutApp::startup() 
+{
+	//Initlize the gizmos singleton class
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
+
 	m_2dRenderer = new aie::Renderer2D();
 
-	// TODO: remember to change this when redistributing a build!
-	// the following path would be used instead: "./font/consolas.ttf"
+	//TODO: remember to change this when redistributing a build!
+	//The following path would be used instead: "./font/consolas.ttf"
 	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
 
 	return true;
 }
 
-void BreakoutApp::shutdown() {
+void BreakoutApp::shutdown()
+{
 
 	delete m_font;
 	delete m_2dRenderer;
 }
 
-void BreakoutApp::update(float deltaTime) {
+void BreakoutApp::update(float deltaTime) 
+{
 
-	// input example
+	//Input example
 	aie::Input* input = aie::Input::getInstance();
 
-	// exit the application
+	//Exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
 }
 
-void BreakoutApp::draw() {
+void BreakoutApp::draw() 
+{
 
-	// wipe the screen to the background colour
+	//Wipe the screen to the background colour
 	clearScreen();
 
-	// begin drawing sprites
+	//Begin drawing sprites
 	m_2dRenderer->begin();
 
-	// draw your stuff here!
+	//Draw your stuff here!
+	static float aspectRatio = 16.0f / 9.0f;
+	aie::Gizmos::draw2D(glm::ortho<float>(-100, 100,
+					   -100 / aspectRatio, 100 / aspectRatio, -1.0f, 1.0f));
 	
-	// output some text, uses the last used colour
+	//Output some text, uses the last used color
 	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
 
-	// done drawing sprites
+	//Done drawing sprites
 	m_2dRenderer->end();
 }
